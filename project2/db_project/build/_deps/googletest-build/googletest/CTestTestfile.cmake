25e295eaec8e004c0c8251b058a94ee7359b2791
박연진 <2020002960@hanyang.ac.kr>
2021-09-30 19:11:06 +0900

    Apply .gitignore

--- project2/db_project/build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/db_project/db_project/build/_deps/googletest-src/googletest
-# Build directory: /root/db_project/db_project/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- project2/db_project/build/test/db_test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/root/db_project/db_project/build/test/db_test[1]_tests.cmake")
-  include("/root/db_project/db_project/build/test/db_test[1]_tests.cmake")
-else()
-  add_test(db_test_NOT_BUILT db_test_NOT_BUILT)
-endif()
